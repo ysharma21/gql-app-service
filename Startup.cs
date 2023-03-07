edd8ebe36f06b01c148dd96a64e6a72b1e74ef79
ysharma21 <yachnasharma@microsoft.com>
2023-03-07 18:15:44 -0500

    first commit

---
+++ Startup.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using GraphQL.Server;
+using GraphQL.Types;
+using appservice_graphql_dotnet.Types;
+using System.Net.Http;
+
+namespace appservice_graphql_dotnet
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<HttpClient>();
+            services.AddSingleton<QuizData>();
+            services.AddSingleton<TriviaQuery>();
+            services.AddSingleton<ISchema, TriviaSchema>();
+
+            services.AddGraphQL(options =>
+            {
+                options.EnableMetrics = true;
+                options.ExposeExceptions = true;
+            })
+            .AddSystemTextJson();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseGraphQL<ISchema>();
+            app.UseGraphQLPlayground();
+        }
+    }
+}
